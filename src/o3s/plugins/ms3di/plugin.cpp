684079a9fadd27223970b303e419485a4aff3dee
Frederic Scherma <frederic.scherma@mailz.org>
2017-12-30 21:01:08 +0100

    ADD: initial for plugin

---
+++ src/o3s/plugins/ms3di/plugin.cpp
@@ -0,0 +1,59 @@
+/**
+ * @brief Ms3di module object
+ * @copyright Copyright (C) 2017 Dream Overflow. All rights reserved.
+ * @author Frederic SCHERMA (frederic.scherma@dreamoverflow.org)
+ * @date 2017-12-30
+ * @details
+ */
+
+#include "plugin.h"
+#include <o3d/core/base.h>
+
+using namespace o3d::studio::ms3di;
+
+extern "C"
+{
+
+O3S_PLUGIN_API o3d::studio::common::Module* o3dstudioPlugin(const o3d::String &name, o3d::DynamicLibrary *library)
+{
+    Plugin *module = new Plugin(name, library);
+    return module;
+}
+
+} // extern "C"
+
+Plugin::Plugin(const String &name, DynamicLibrary *library) :
+    DynamicModule(name, library)
+{
+    m_properties["section"] = "o3s::plugin::importer";
+    m_properties["verbose"] = "Ms3d Importer";
+
+    m_properties["vendor"] = "Dream Overflow";
+    m_properties["group"] = "Official extensions";
+    m_properties["url"] = "http://o3dstudio.dreamoverflow.org/ms3di";
+    m_properties["plateforms"] = "all";
+
+    m_properties["version"] = "1.0.0";
+    m_properties["description"] = "A Milkshape 3D models importer";
+    m_properties["copyright"] = "(C) 2017 Dream Overflow";
+    m_properties["license"] = "Proprietary";
+
+    m_properties["dependencies"] = "";
+}
+
+Plugin::~Plugin()
+{
+
+}
+
+o3d::Bool Plugin::start()
+{
+    m_started = True;
+    return True;
+}
+
+o3d::Bool Plugin::stop()
+{
+    m_started = False;
+    return False;
+}
---
+++ src/o3s/plugins/ms3di/plugin.h
@@ -0,0 +1,36 @@
+/**
+ * @brief dummy module object
+ * @copyright Copyright (C) 2017 Dream Overflow. All rights reserved.
+ * @author Frederic SCHERMA (frederic.scherma@dreamoverflow.org)
+ * @date 2017-10-19
+ * @details
+ */
+
+#ifndef _O3DS_MS3DI_PLUGIN_H
+#define _O3DS_MS3DI_PLUGIN_H
+
+#include <o3d/studio/common/dynamicmodule.h>
+
+namespace o3d {
+namespace studio {
+namespace ms3di {
+
+class O3S_PLUGIN_API Plugin : public studio::common::DynamicModule
+{
+public:
+
+    Plugin(const String &name, DynamicLibrary *library);
+    virtual ~Plugin();
+
+    virtual Bool start();
+    virtual Bool stop();
+
+private:
+
+};
+
+} // namespace ms3di
+} // namespace studio
+} // namespace o3d
+
+#endif // _O3DS_MS3DI_PLUGIN_H
